18f3d2858091785658d74f58c1e8859970c82271
dag242 <121731315+dag242@users.noreply.github.com>
2023-09-16 20:00:07 -0400

    img edit needed

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,7 +3,7 @@ import Typewriter from 'typewriter-effect'
 import React from 'react'
 import 'react-slideshow-image/dist/styles.css'
 import { Fade, Zoom, Slide } from 'react-slideshow-image'
-
+import Turtle from '../../assets/images/turtle.png'
 
 
 const Projects = () => {
@@ -23,18 +23,13 @@ const Projects = () => {
                         }}
                         />
                     </h1>
-                <p>
-                    Created arcade games called Star Collector, Asteroids, Plane Dodger, Brick Breaker, using Javafx library
-                </p>
-                <p>
-                    Games will be added here soon
-                </p>
-                <p>
-                    I have acquired proficiency in several programming languages, including React.js, Java,C, Python, and C++.
-                    My knowledge in these languages has been cultivated through a combination of formal classroom education
-                    and self-guided learning. This diverse skill set enables me to work effectively on a wide range games and 
-                    websites and tackle various programming challenges with confidence.
-                </p>
+                
+                </div>
+                <div className = "carousel-item">
+                    <img src = {Turtle} alt="turtle"></img>
+                    <div>
+                        
+                    </div>
                 </div>
 
             </div>
